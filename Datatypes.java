4dd9c60b89bd32ebf9bb4e2ec0009dc5923eebb7
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-02-15 15:54:20 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,10 @@
+public class Datatypes{
+	public static void main(String[] args){
+		int age=20;
+		String name = "Supraja";
+		float salary=10000.50f;
+		double bank=10000000000000.75;
+		System.out.println("Hey my name is "+name+" My age is "+age+"My salary is "+salary);
+	}
+
+}
