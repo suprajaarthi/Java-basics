aba04f647e5a296107b28ae6da15836a0220470f
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-02-16 10:43:24 +0530

    Add files via upload

---
+++ StringCompare.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class StringCompare {
+	public static void main (String[] args) {
+		String s1="Hello";
+		String s2="Hello";
+		String s3="hELlo";
+		String s4="BYE";
+		System.out.println((s1).compareTo(s2));
+		System.out.println((s1).compareTo(s3));
+		System.out.println((s1).compareTo(s4));
+
+
+}
+}
