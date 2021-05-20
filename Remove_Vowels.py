a2798bf2de5aa55289c0f18ca3da34bf9c44002d
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-20 23:34:37 +0530

    Create Remove_Vowels.py

---
+++ Remove_Vowels.py
@@ -0,0 +1,13 @@
+# https://ide.geeksforgeeks.org/idupVVkzHR
+
+class GFG {
+	public static void main (String[] args) {
+        String s="Examination";
+        s=s.replaceAll("[aeiouAEIOU]","");
+        System.out.println(s);
+        
+	}
+} 
+# O/P: xmntn
+
+
