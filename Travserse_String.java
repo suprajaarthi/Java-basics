c3577996227873355538c9cf953725dc4c19378b
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-20 18:21:29 +0530

    Create Travserse_String.java

---
+++ Travserse_String.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+        String a = "Hello World";
+        String[] arr = a.split(" ");
+        for (String x:arr)
+        {
+            int i=0;
+            for(Character ch : x.toCharArray())
+            {   
+                i+=1;
+                if(i%2==0)
+                {
+                    System.out.print(Character.toUpperCase(ch));
+                    
+                }
+            }
+        }        
+	}
+}
+
+
+// https://ide.geeksforgeeks.org/dZtuNf9V6w
