848a3176e4c704cba1546d87a9121a5ac89256c5
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-20 18:01:40 +0530

    Create Str-CharArray.java

---
+++ Str-CharArray.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		String s = "APPLE";
+		char[] ch = s.toCharArray();
+		System.out.println(ch);
+		for (int i=0;i<ch.length ;i++ )
+		{
+		  //  if(ch[i] )
+		  System.out.print(ch[i]+" ");
+		}
+	}
+}
+
+// https://ide.geeksforgeeks.org/Vzp3ge9Hax
