ff255640c8ee56a99470e15e9bfb8401e6025dd6
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-20 18:39:47 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+        String a = "Hello World";
+        StringBuilder sb = new StringBuilder();
+        sb.append(a);
+        sb.reverse();
+        System.out.println(sb);
+	}
+}
+// https://ide.geeksforgeeks.org/2HygsrxEOL
+// op : dlroW olleH
