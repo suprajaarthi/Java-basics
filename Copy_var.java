acf63ec26501275ffee600dcf6b5552a762933b3
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2022-07-21 08:53:26 +0530

    Create Copy_var.java

---
+++ Copy_var.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+    int myAge=30;
+    int herAge=myAge; 
+    System.out.println(herAge);
+    }
+    }
+    
+    
+ # OP : 30
